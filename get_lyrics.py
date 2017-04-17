d709d87d557fc60c3811c980a38c59f266ba690f
Theo Massard <massar_t@etna-alternance.net>
2017-04-17 19:34:44 +0200

    [UPD] Documented code, removed linting errors, added some try/excepts

--- get_lyrics.py
+++ get_lyrics.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 # coding: utf-8
+'''
+get_lyrics.py
+
+CLI utility allowing you to get a song's lyrics
+based on the Artist's name and the Song's name
+
+Uses the Genius API
+'''
 import argparse
 import json
 import sys
 import requests
+from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
 
 
@@ -16,8 +25,7 @@ def get_config(config_file='config.json'):
     """
     with open(config_file) as json_data_file:
         data = json.load(json_data_file)
-    a = data.get("token")
-    return a
+    return data.get("token")
 
 
 BASE_URL = "http://api.genius.com"
@@ -25,46 +33,63 @@ HEADERS = {'Authorization': get_config()}
 
 
 def check_args():
+    '''Collects arguments (Song name, Artist name)'''
     parser = argparse.ArgumentParser()
-    parser.add_argument("song")
-    parser.add_argument("artist")
+    parser.add_argument(
+        "-s",
+        "--song",
+        type=str,
+        required=True)
+    parser.add_argument(
+        "-a",
+        "--artist",
+        type=str,
+        required=True)
     args = parser.parse_args()
     return args.song, args.artist
 
 
 def get_song_id_from_name(song_name, artist_name):
+    '''Does a GET request to the API and returns the artist's ID'''
     search_url = BASE_URL + "/search"
     data = {'q': song_name + '+' + artist_name}
-    response = requests.get(search_url, params=data, headers=HEADERS)
-    json = response.json()
+    try:
+        response = requests.get(search_url, params=data, headers=HEADERS)
+    except RequestException:
+        sys.exit(1)
+    resp = response.json()
     artist = artist_name.upper()
     if response.status_code != 200:
         sys.exit("An error occured. (Is your token valid ?)")
-    for res in json["response"]["hits"]:
+    for res in resp["response"]["hits"]:
         if res["result"]["primary_artist"]["name"].upper() == artist:
             return res["result"]["id"]
-    return 0
 
 
 def get_web_path_from_song_id(song_id):
+    '''GET request to get the path based on the song ID'''
     search_url = BASE_URL + "/songs/" + str(song_id)
     response = requests.get(search_url, headers=HEADERS)
     if response.status_code != 200:
         sys.exit("Combination Song/Artist not found")
-    json = response.json()
-    return json["response"]["song"]["path"]
+    resp = response.json()
+    try:
+        return resp["response"]["song"]["path"]
+    except KeyError:
+        return
 
 
 def get_lyrics_from_path(path):
+    '''GET request to Genius API, then parses lyrics'''
     url = "http://genius.com" + path
     response = requests.get(url)
-    bs = BeautifulSoup(response.text, "html.parser")
-    [h.extract() for h in bs('script')]
-    lyrics = bs.find("lyrics").get_text()
+    bs_parsed = BeautifulSoup(response.text, "html.parser")
+    lyrics = bs_parsed.find("lyrics").get_text()
     return lyrics
 
 
 def main():
+    '''Main Function'''
     song_name, artist_name = check_args()
     song_id = get_song_id_from_name(song_name, artist_name)
     web_path = get_web_path_from_song_id(song_id)
